74a875da87e9444254fedcdb37648b0fe008c4ec
MareliLourens <221119@virtualwindow.co.za>
2024-03-11 10:12:32 +0200

    Project Setup
    The initial setup for the project

---
+++ andME_Entertainment/src/app/components/inventory-stock/inventory-stock.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-stock',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-stock.component.html',
+  styleUrl: './inventory-stock.component.css'
+})
+export class InventoryStockComponent {
+
+}
