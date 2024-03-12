b3eab79905c8d23a217752fafa51544340dce87e
DupieM <221326@virtualwindow.co.za>
2024-03-12 20:28:01 +0200

    Authentication page frontend

--- andME_Entertainment/src/app/components/authentication/authentication.component.ts
+++ andME_Entertainment/src/app/components/authentication/authentication.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule  ],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.css'
 })
