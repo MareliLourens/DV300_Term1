a1032ca411ff528d34b88fe95f17ab7571e16edd
DupieM <221326@virtualwindow.co.za>
2024-04-04 10:01:19 +0200

    creating card

--- andME_Entertainment/src/app/services/inventory-one.service.ts
+++ andME_Entertainment/src/app/services/inventory-one.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InventoryOne } from '../models/inventory-one.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryOneService {
 
-  constructor() { }
+   // add httpClient functionality to this service
+   constructor(private http: HttpClient) {}
+
+   private baseUrl = "http://localhost:3000/inventory"
+ 
+   //get all the inventory items
+   getAllInventory(): Observable<InventoryOne[]>{
+     return this.http.get<InventoryOne[]>(this.baseUrl)
+   }
+
 }
