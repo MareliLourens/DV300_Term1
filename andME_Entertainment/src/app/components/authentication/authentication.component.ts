717f00edcbe9753f9793de72832ef812fdd249d1
MareliLourens <221119@virtualwindow.co.za>
2024-04-07 11:36:56 +0200

    Revert "Error on displaying unique_question"
    This reverts commit be4e98859414fd1a99ed5b673702cbfadda5680c.

--- andME_Entertainment/src/app/components/authentication/authentication.component.ts
+++ andME_Entertainment/src/app/components/authentication/authentication.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../services/auth.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -10,39 +10,27 @@ import { Router } from '@angular/router';
   imports: [MatCardModule, ReactiveFormsModule],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.css'
-})
-export class AuthenticationComponent implements OnInit {
+}
+)
+export class AuthenticationComponent {
   constructor(private service: AuthService, private router: Router) { }
 
   login = new FormGroup({
     phone_number: new FormControl('', Validators.required),
     unique_answer: new FormControl('', Validators.required)
-  });
-
-  uniqueQuestion: string = '';
-
-  ngOnInit() {
-    // Fetch the unique question when the component initializes
-    this.service.getUniqueQuestion().subscribe(question => {
-      this.uniqueQuestion = question;
-    });
-  }
+  })
 
   onSubmit() {
-    if (this.login.value.phone_number !== "" && this.login.value.unique_answer !== "") {
+
+    if (this.login.value.phone_number != "" && this.login.value.unique_answer != "") {
+
       this.service.loginUser(this.login.value.phone_number!, this.login.value.unique_answer!)
-        .subscribe((success) => {
-          if (success) {
-            this.router.navigateByUrl("/locations");
-          }
-        });
+      .subscribe((success) => {
+        if(success){
+          this.router.navigateByUrl("/locations");
+        }
+      })
     }
   }
-
-  revealQuestion() {
-    // Fetch the unique question again when the button is clicked
-    this.service.getUniqueQuestion().subscribe(question => {
-      this.uniqueQuestion = question;
-    });
-  }
 }
+
--- andME_Entertainment/src/app/services/auth.service.ts
+++ andME_Entertainment/src/app/services/auth.service.ts
@@ -1,45 +1,29 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+
   constructor(private http: HttpClient) { }
 
   private loginUrl = "http://localhost:3000/users";
-  private questionUrl = "http://localhost:3000/users"; // Assuming you have an endpoint to fetch the unique question
 
   loginUser(phone_number: string, unique_answer: string): Observable<boolean> {
     return this.http.post<any>(this.loginUrl, { phone_number, unique_answer }).pipe(
-      catchError(this.handleError), // Add error handling here
-      map(response => {
+      tap(response => {
         if (response) {
-          console.log(response);
+          console.log(response)
           sessionStorage.setItem("currentUser", JSON.stringify(response));
-          return true;
         }
-        return false;
       })
     );
   }
 
-  getUniqueQuestion(): Observable<string> {
-    return this.http.get<any>(this.questionUrl).pipe(
-      map((response: { unique_question: any; }) => response.unique_question),
-      catchError(this.handleError) // Add error handling here
-    );
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    console.error('An error occurred:', error.error.message || error.statusText);
-    return throwError('Something bad happened; please try again later.');
-  }
-
   logout(){
-    sessionStorage.removeItem("user");
+    sessionStorage.removeItem("user")
   }
 }
