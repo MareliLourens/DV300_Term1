f09dc6caed9a230bd8c172f913e49915b075dc48
DupieM <221326@virtualwindow.co.za>
2024-04-07 11:58:19 +0200

    Sum on locations page

--- andME_Entertainment/src/app/components/locations/locations.component.ts
+++ andME_Entertainment/src/app/components/locations/locations.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { InventoryOneService } from '../../services/inventory-one.service';
+import { Inventory_One } from '../../models/inventory-one.model';
 
 
 @Component({
@@ -10,6 +12,47 @@ import {MatCardModule} from '@angular/material/card';
   styleUrl: './locations.component.css'
 })
 export class LocationsComponent {
+
+  constructor(private service: InventoryOneService) {}
+
+  
+  inventory: Inventory_One[] = [
+    {
+      id: 0,
+      name: "HTML",
+      image: "assets/assests/BE (Deluxe Edition)/CD.png",
+      amount_avaible: 0,
+      main_category: "Album BE",
+    },
+  ] 
+
+  sum = 0
+
+  number = this.inventory.forEach(a => this.sum += a.amount_avaible);
+  
+  ngOnInit(){
+    this.service.getAllInventory().subscribe((data) => {
+      console.log(data);
+      this.inventory = data
+    })
+  }
+
+  // totalNumber = 0
+
+  // selectedInventory?: Inventory_One;
+
+  // setSelectedItem(inventory: Inventory_One) {
+
+  //   this.totalNumber = this.selectedInventory!.amount_avaible
+
+  //   console.log(this.totalNumber)
+
+  // }
+
+  
+
+  
+  
 
 
 }
