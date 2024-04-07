a027ce1488ea9465a327f677a95b007cdfc6261e
MareliLourens <221119@virtualwindow.co.za>
2024-04-07 12:15:17 +0200

    Landing Page + Readme

--- andME_Entertainment/src/app/app.component.ts
+++ andME_Entertainment/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, Router, RouterOutlet } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { CommonModule } from '@angular/common';
 
@@ -7,25 +7,16 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root', 
   standalone: true,
-<<<<<<< Updated upstream
-  imports: [RouterOutlet, LandingComponent, CommonModule, RouterLink, RouterLinkActive], //import any components or angular material
-  templateUrl: './app.component.html', //render html
-  styleUrl: './app.component.css' //styling
-=======
   imports: [RouterOutlet, LandingComponent, CommonModule, RouterLink, RouterLinkActive ],
   templateUrl: './app.component.html', 
   styleUrl: './app.component.css' 
->>>>>>> Stashed changes
 })
 export class AppComponent {
   title = '&ME Entertainment';
-<<<<<<< Updated upstream
-=======
 
   
   constructor(public router: Router) {}
 
   
   
->>>>>>> Stashed changes
 }
